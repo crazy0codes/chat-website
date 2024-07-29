dec94e67e0141238d4b51685005c2a68040e5bb8
crazy0codes <madhanpanja@gmail.com>
2024-07-29 20:02:12 +0530

    Ignore build.zip in .gitignore and update CardTitle component in card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -19,10 +19,10 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 CardHeader.displayName = "CardHeader"
 
 const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
-    {...props} />
+<h3 ref={ref} aria-readonly="true" className={cn("text-2xl font-semibold leading-none tracking-tight", className)}>
+  {props.children}
+</h3>
+
 ))
 CardTitle.displayName = "CardTitle"
 
